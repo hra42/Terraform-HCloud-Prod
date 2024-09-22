7c041a6bc3423261c08ebb0faac20151b0c67df6
Henry Rausch <44985923+hra42@users.noreply.github.com>
2024-09-22 22:20:36 +0200

    feat: Update server config and gitignore
    Update server configuration for production environment:
    - Change server name to "Prod"
    - Upgrade server type to cx22
    - Update labels for production
    
    Improve .gitignore:
    - Add .aider* and *.hcl to ignored files
    - Remove trailing whitespace
    
    Format Terraform files for better readability and consistency

--- main.tf
+++ main.tf
@@ -1,85 +1,85 @@
 # Tell terraform to use the provider and select a version.
 terraform {
-    required_providers {
-        hcloud = {
-            source = "hetznercloud/hcloud"
-            version = "~> 1.45"
-        }
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
     }
+  }
 }
 
 # Set Hetzner Cloud token via environment variable
 variable "hcloud_token" {
-    type = string
-    sensitive = true
-    description = "Hetzner Cloud API Token"
+  type        = string
+  sensitive   = true
+  description = "Hetzner Cloud API Token"
 }
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-    token = var.hcloud_token
+  token = var.hcloud_token
 }
 
 # Create a network
 resource "hcloud_network" "nixos_network" {
-    name = "NixOS-Network"
-    ip_range = "10.0.0.0/16"
+  name     = "NixOS-Network"
+  ip_range = "10.0.0.0/16"
 }
 
 # Create a sub-network
 resource "hcloud_network_subnet" "nixos_subnet" {
-    type = "cloud"
-    network_id = hcloud_network.nixos_network.id
-    ip_range = "10.0.1.0/24"
-    network_zone = "eu-central"
+  type         = "cloud"
+  network_id   = hcloud_network.nixos_network.id
+  ip_range     = "10.0.1.0/24"
+  network_zone = "eu-central"
 }
 
 # Fetch SSH key from Hetzner Cloud
 data "hcloud_ssh_key" "prod-ssh-key" {
-  name = "prod"
+  name = "Prod"
 }
 
 # Create NixOS Server
 resource "hcloud_server" "nixos_server" {
-    name        = "NixOS-Server"
-    image       = "ubuntu-22.04"
-    server_type = "cx11"
-    location    = "fsn1"
-    user_data = file("user_data.sh")
-    ssh_keys  = [data.hcloud_ssh_key.prod-ssh-key.id]
+  name        = "Prod"
+  image       = "ubuntu-22.04"
+  server_type = "cx22"
+  location    = "fsn1"
+  user_data   = file("user_data.sh")
+  ssh_keys    = [data.hcloud_ssh_key.prod-ssh-key.id]
 
-    labels = {
-        "env" = "test"
-        "os" = "nixos"
-    }
+  labels = {
+    "env" = "prod"
+    "os"  = "nixos"
+  }
 
-    public_net {
-        ipv4_enabled = true
-        ipv6_enabled = true
-    }
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
 
-    network {
-        network_id = hcloud_network.nixos_network.id
-        ip = "10.0.1.10"
-    }
+  network {
+    network_id = hcloud_network.nixos_network.id
+    ip         = "10.0.1.10"
+  }
 
-    depends_on = [
-        hcloud_network_subnet.nixos_subnet
-    ]
+  depends_on = [
+    hcloud_network_subnet.nixos_subnet
+  ]
 }
 
 output "server_ipv4" {
-    value = hcloud_server.nixos_server.ipv4_address
-    description = "The IPv4 address of the NixOS Server"
+  value       = hcloud_server.nixos_server.ipv4_address
+  description = "The IPv4 address of the NixOS Server"
 }
 
 output "server_ipv6" {
-    value = hcloud_server.nixos_server.ipv6_address
-    description = "The IPv6 address of the NixOS Server"
+  value       = hcloud_server.nixos_server.ipv6_address
+  description = "The IPv6 address of the NixOS Server"
 }
 
 output "next_steps" {
-    value = <<EOT
+  value       = <<EOT
 
 Next steps:
 1. Wait for a few minutes to allow the NixOS installation to complete.
@@ -89,5 +89,5 @@ Next steps:
 Remember that the server starts as Ubuntu and then converts to NixOS.
 The conversion process may take a few minutes to complete.
 EOT
-    description = "Instructions for next steps after Terraform apply"
+  description = "Instructions for next steps after Terraform apply"
 }
